f93486c6e4fd212aa19dd6415126a130a3d3f366
restep <restep@163.com>
2019-02-23 14:49:48 +0800

    welcome to core-jav

---
+++ src/com/restep/v1ch02/Welcome.java
@@ -0,0 +1,18 @@
+package com.restep.v1ch02;
+
+/**
+ * @author restep
+ * @date 2019/2/23
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        String[] strArr = new String[3];
+        strArr[0] = "a";
+        strArr[1] = "b";
+        strArr[2] = "c";
+
+        for (String str : strArr) {
+            System.out.println(str);
+        }
+    }
+}
